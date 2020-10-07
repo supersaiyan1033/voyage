c61620906a0ee4e945351827e9e037719d1f3e61
revanththota <cse190001063@iiti.ac.in>
2020-10-07 10:32:10 +0530

    addition of .sql file and change of database in settings.py

--- voyage/voyage/settings.py
+++ voyage/voyage/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'voyage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'voyage',
+        'USER':'nitin',
+        'PASSWORD':'roomno605',
+        'HOST': 'localhost',
+        'PORT':'3306',
+
     }
 }
 
