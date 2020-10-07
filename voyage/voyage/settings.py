d61aed48d88439bd84aadd987c0ed97f9987aae2
supersaiyan1033 <62352423+supersaiyan1033@users.noreply.github.com>
2020-10-07 13:10:36 +0530

    revert

--- voyage/voyage/settings.py
+++ voyage/voyage/settings.py
@@ -78,13 +78,8 @@ WSGI_APPLICATION = 'voyage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'voyage',
-        'USER':'nitin',
-        'PASSWORD':'roomno605',
-        'HOST': 'localhost',
-        'PORT':'3306',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
