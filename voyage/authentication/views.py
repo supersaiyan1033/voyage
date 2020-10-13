e11c5056b6c8ee98c3adb27737dae161b234c78e
supersaiyan1033 <62352423+supersaiyan1033@users.noreply.github.com>
2020-10-13 15:39:32 +0530

    added error handling and updated user page

--- voyage/authentication/views.py
+++ voyage/authentication/views.py
@@ -3,6 +3,7 @@ from django import forms
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.db import connection
+from django.contrib import messages
 import bcrypt
 import hashlib, sys
 import base64
@@ -18,17 +19,17 @@ def Log_In(request):
           cursor = connection.cursor()
           cursor.execute("""SELECT * FROM users WHERE email= %s""",[email])
           row = cursor.fetchall()
-          print(row[0][6])
           if cursor.rowcount ==1:
              dbpassword = row[0][6]
              print(dbpassword)
              if bcrypt.checkpw(password.encode('utf8'),dbpassword.encode('utf8')):
-                 print('user exists login successfully')
+                 messages.success(request,'Login successful!!')
                  return render (request,'authentication/user.html')
              else:
-               print('incorrect password')
+               messages.success(request,'incorrect password please try again!!')
+               return render(request,'authentication/login.html')
           else:
-            print('invalid credentials')
+            messages.success(request,'Account does not exist with the entered credentials!! signup to create an account')
             return render(request,'authentication/login.html')
      else:
        return render(request,'authentication/login.html')
@@ -47,8 +48,14 @@ def Sign_Up(request):
           password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt(rounds=12))
 
           cursor = connection.cursor()
-          cursor.execute("""INSERT INTO users (firstname,lastname,gender,address,mobileno,email,password,DOB) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""", (firstname ,lastname,gender,address,mobileno,email,password,DOB))
-          return render(request,'authentication/signup.html')    
+          cursor.execute("""SELECT * FROM users WHERE email = %s""",[email])
+          row = cursor.fetchall()
+          if cursor.rowcount==0:
+             cursor.execute("""INSERT INTO users (firstname,lastname,gender,address,mobileno,email,password,DOB) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""", (firstname ,lastname,gender,address,mobileno,email,password,DOB))
+             return render(request,'authentication/profile.html')  
+          else:
+             messages.success(request,'User with the entered email already exists!!!')
+             return render(request,'authentication/signup.html')
      else:        
         return render(request,'authentication/signup.html')
 
