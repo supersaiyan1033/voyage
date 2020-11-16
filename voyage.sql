45fcb405d0f78c0a07e5d2172bf95c5945b40bfc
supersaiyan1033 <62352423+supersaiyan1033@users.noreply.github.com>
2020-11-16 21:21:45 +0530

    Update voyage.sql

--- voyage.sql
+++ voyage.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `auth_group`
 --
-
+CREATE DATABASE voyage;
+USE voyage;
 DROP TABLE IF EXISTS `auth_group`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
